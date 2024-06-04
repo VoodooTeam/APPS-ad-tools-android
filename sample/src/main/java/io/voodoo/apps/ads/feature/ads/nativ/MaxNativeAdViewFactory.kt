41b06fc13c4ec043a98f053ee4fcccd6c96a44fe
Yann Badoual <yann.badoual@gmail.com>
2024-06-04 16:04:19 +0200

    Handle button transition

--- sample/src/main/java/io/voodoo/apps/ads/feature/ads/nativ/MaxNativeAdViewFactory.kt
+++ sample/src/main/java/io/voodoo/apps/ads/feature/ads/nativ/MaxNativeAdViewFactory.kt
@@ -1,11 +1,18 @@
 package io.voodoo.apps.ads.feature.ads.nativ
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.Rect
+import android.view.View
+import android.view.View.OnAttachStateChangeListener
 import android.view.ViewGroup
+import androidx.core.view.postDelayed
 import com.applovin.mediation.nativeAds.MaxNativeAdView
 import com.applovin.mediation.nativeAds.MaxNativeAdViewBinder
 import io.voodoo.apps.ads.R
 import io.voodoo.apps.ads.applovin.nativ.MaxNativeAdViewFactory
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
 class MaxNativeAdViewFactory : MaxNativeAdViewFactory {
 
@@ -26,6 +33,56 @@ class MaxNativeAdViewFactory : MaxNativeAdViewFactory {
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
+
+            addOnAttachStateChangeListener(object : OnAttachStateChangeListener {
+                override fun onViewAttachedToWindow(v: View) {
+                    doOnVisible(pollInterval = 1.seconds) {
+                        updateState(visible = true)
+                    }
+                }
+
+                override fun onViewDetachedFromWindow(v: View) {
+                    updateState(visible = false)
+                }
+            })
         }
+    }
+
+    private fun MaxNativeAdView.updateState(visible: Boolean) {
+        if (visible) {
+            postDelayed(3_000) {
+                callToActionButton.isActivated = true
+            }
+        } else {
+            // Reset for next ad
+            callToActionButton.isActivated = false
+        }
+    }
+
+    private fun MaxNativeAdView.doOnVisible(
+        pollInterval: Duration,
+        body: () -> Unit
+    ) {
+        if (isVisible()) {
+            body()
+        } else {
+            // Wait for active status
+            postDelayed(pollInterval.inWholeMilliseconds) {
+                doOnVisible(pollInterval, body)
+            }
+        }
+    }
+
+    private fun View.isVisible(): Boolean {
+        if (!isShown) {
+            return false
+        }
+        val actualPosition = Rect()
+        val isGlobalVisible = getGlobalVisibleRect(actualPosition)
+        val (screenWidth, screenHeight) = Resources.getSystem().displayMetrics.let {
+            it.widthPixels to it.heightPixels
+        }
+        val screen = Rect(0, 0, screenWidth, screenHeight)
+        return isGlobalVisible && Rect.intersects(actualPosition, screen)
     }
 }
